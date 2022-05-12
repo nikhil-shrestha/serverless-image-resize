cf4edc349d8b318cb586d4ab2219f76cde52bfc1
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2022-05-12 12:57:15 +0545

    updated image resize

--- index(1).js
+++ index(1).js
@@ -1,29 +1,77 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
 const sharp = require('sharp');
 
-function processImage(imageStream, width, height) {
-  return sharp()
-    .resize({
-      width,
-      height,
-      fit: 'cover',
-      withoutEnlargement: true
-    })
-    .pipe(imageStream);
-}
+const THUMB_MAX_WIDTH = 200;
+const THUMB_MAX_HEIGHT = 200;
 
-exports.main = (req, res) => {
-  const filename = req.query.f;
+const gcs = new Storage();
 
-  if (!filename) {
-    res.sendStatus(400).send('No file specified');
-    return;
+/**
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
+ * Sharp.
+ */
+exports.generateThumbnail = functions.storage.object().onFinalize((object) => {
+  const fileBucket = object.bucket; // The Storage bucket that contains the file.
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('image/')) {
+    functions.logger.log('This is not an image.');
+    return null;
   }
 
-  const width = praseInt(req.query.w);
-  const height = praseInt(req.query.h);
+  // Get the file name.
+  const fileName = path.basename(filePath);
+  // Exit if the image is already a thumbnail.
+  if (fileName.startsWith('thumb_')) {
+    functions.logger.log('Already a Thumbnail.');
+    return null;
+  }
 
-  const file = Storage.bucket(process.env.BUCKET_NAME).file(filename);
-  const stream = file.createReadStream();
+  // Download file from bucket.
+  const bucket = gcs.bucket(fileBucket);
 
-  processImage(stream, width, height).pipe(res);
-};
+  const metadata = {
+    contentType: contentType
+  };
+  // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+  const thumbFileName = `thumb_${fileName}`;
+  const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+  // Create write stream for uploading thumbnail
+  const thumbnailUploadStream = bucket
+    .file(thumbFilePath)
+    .createWriteStream({ metadata });
+
+  // Create Sharp pipeline for resizing the image and use pipe to read from bucket read stream
+  const pipeline = sharp();
+  pipeline
+    .resize(THUMB_MAX_WIDTH, THUMB_MAX_HEIGHT)
+    .max()
+    .pipe(thumbnailUploadStream);
+
+  bucket.file(filePath).createReadStream().pipe(pipeline);
+
+  return new Promise((resolve, reject) =>
+    thumbnailUploadStream.on('finish', resolve).on('error', reject)
+  );
+});
--- index.js
+++ index.js
@@ -1,81 +1,148 @@
+// Create clients outside of the handler
+
+// Create a client to read objects from S3
 const AWS = require('aws-sdk');
+
+//configuring the AWS environment
+AWS.config.update({
+  accessKeyId: 'AKIATSWZCGZ725IJ4MCM',
+  secretAccessKey: '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'
+});
+
+const s3 = new AWS.S3();
+
+// Libraries for image resize.
 const sharp = require('sharp');
 const stream = require('stream');
 
-const width = 400;
+const width = 500;
 const prefix = `${width}w`;
 
-const S3 = new AWS.S3();
-
-// Read stream for downloading from S3
+// Function to create a read stream to an S3 object.
 function readStreamFromS3({ Bucket, Key }) {
-  return S3.getObject({ Bucket, Key }).createReadStream();
+  return s3.getObject({ Bucket, Key }).createReadStream();
 }
 
-// Write stream for uploading to S3
+// Function to create a write stream to S3.
 function writeStreamToS3({ Bucket, Key }) {
   const pass = new stream.PassThrough();
-
   return {
     writeStream: pass,
-    upload: S3.upload({
-      Key,
-      Bucket,
-      Body: pass
-    }).promise()
+    uploadFinished: s3
+      .upload({
+        Body: pass,
+        Bucket,
+        Key
+      })
+      .promise()
   };
 }
 
-// Sharp resize stream
-function streamToSharp(width) {
+// Get size from stream (sharp)
+function getSizeFromStream() {
+  const pass = new stream.PassThrough();
+  return sharp(pass)
+    .metadata()
+    .then((metadata) => {
+      return {
+        writeStream: pass,
+        width: metadata.width,
+        height: metadata.height
+      };
+    });
+}
+
+// Resize image stream (sharp).
+function resizeStream({ width }) {
   return sharp().resize(width);
 }
 
-exports.handler = async (event, context) => {
-  const s3Record = event.Records[0].s3;
+/**
+ * A Lambda function that logs the payload received from S3.
+ */
+const handler = async () => {
+  console.log('here...');
 
-  // Read options from the event parameter.
-  const Key = s3Record.object.key;
-  const Bucket = s3Record.bucket.name;
-  const sanitizedKey = key.replace(/\+/g, ' ');
-  const keyWithoutExtension = sanitizedKey.replace(/.[^.]+$/, '');
+  // const record = event.Records[0].s3;
 
-  // Infer the image type from the file suffix.
-  const typeMatch = sanitizedKey.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-    context.fail('Could not determine the image type.');
+  // const Key = record.object.key;
+  // const Bucket = record.bucket.name;
+
+  const Key = 'abc/destination/0203defa-f86c-4c03-bc94-ffa229a7967a';
+  const Bucket = 'exclusivegulets';
+
+  try {
+    // First, copy the original to a new file.
+    const sanitizedKey = Key.replace(/\+/g, ' ');
+    const keyWithoutExtension = sanitizedKey.replace(/.[^.]+$/, '');
+
+    // Show keys and stuff.
+    console.log('Original Key: ' + keyWithoutExtension);
+    //console.log('prefix:' + prefix);
+
+    // Infer the image type from the file suffix.
+    const typeMatch = sanitizedKey.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+      console.error('Could not determine the image type.');
+      // context.fail('Could not determine the image type.');
+      return;
+    }
+
+    // Check that the image type is supported
+    const fileExt = typeMatch[1].toLowerCase();
+    if (
+      fileExt != 'jpg' &&
+      fileExt != 'png' &&
+      fileExt != 'gif' &&
+      fileExt != 'webp'
+    ) {
+      console.error(`Unsupported image type: ${fileExt}`);
+      // context.fail(`Unsupported image type: ${fileExt}`);
+      return;
+    }
+
+    console.log('File extension: ' + fileExt);
+
+    let newKey = '';
+    if (fileExt) {
+      newKey += `${keyWithoutExtension}_${prefix}.${fileExt}`;
+    } else {
+      newKey += `${keyWithoutExtension}_${prefix}`;
+    }
+    console.log('newKey: ' + newKey);
+
+    // Create a pipeline inline to read the metadata.
+    //let origWidth;
+    //let origHeight;
+    //const pipeline = sharp().metadata().then(metadata =>{
+    //    origWidth = metadata.width;
+    //    origHeight = metadata.height;
+    //});
+
+    // Have to determine the original width and height.
+    // Can that be done through a stream, or does it have to be done
+    // before processing?
+    // Compute ratio of width/height. width will always be 500 pixels in the end.
+    const readStream = readStreamFromS3({ Bucket, Key });
+    const resizeStreamVar = resizeStream({ width });
+    const { writeStream, uploadFinished } = writeStreamToS3({
+      Bucket,
+      Key: newKey
+    });
+
+    // Trigger the stream.
+    readStream.pipe(resizeStreamVar).pipe(writeStream);
+
+    const uploadedDataResized = await uploadFinished;
+
+    console.log('Image was resized:', JSON.stringify(uploadedDataResized));
+
+    // context.succeed();
+  } catch (error) {
+    console.log('Error calling S3 getObject: ' + error);
+    console.error('Error calling S3 getObject:' + error);
+    // context.fail('Error calling S3 getObject: ' + error);
   }
-
-  // Check that the image type is supported
-  const extension = typeMatch[1].toLowerCase();
-  if (
-    extension != 'jpg' &&
-    extension != 'png' &&
-    extension != 'gif' &&
-    extension != 'webp'
-  ) {
-    context.fail(`Unsupported image type: ${extension}`);
-  }
-
-  // Create the new filename with the dimensions
-  const newKey = `${keyWithoutExtension}_${prefix}.${extension}`;
-
-  // Stream to read the file from the bucket
-  const readStream = readStreamFromS3({ Key, Bucket });
-
-  // Stream to resize the image
-  const resizeStream = streamToSharp(width);
-  // Stream to upload to the bucket
-  const { writeStream, upload } = writeStreamToS3({
-    Bucket,
-    Key: newKey
-  });
-
-  // Trigger the streams
-  readStream.pipe(resizeStream).pipe(writeStream);
-
-  // Wait for the file to upload
-  await upload;
-
-  context.succeed();
 };
+
+handler();
